8a4647d25ed702dbf6f7dfe09585f6e280875074
hqnguyen <hqnguyen@sfu.ca>
2021-12-25 13:32:52 -0800

    add responsive resume and project

--- src/pages/resume.js
+++ src/pages/resume.js
@@ -1,20 +1,13 @@
 import React from "react";
 import NavBar from "../components/NavBar/NavBar";
 import image from '../../public/static/resume.jpg'
-
-const styles = {
-    'display': 'block',
-    'marginLeft': 'auto',
-    'marginRight': 'auto',
-    'width': '50%',
-    'padding': '20px'
-}
+import * as styles from '../styles/global.module.css'
 
 export default function Component() {
     return (
         <main>
             <NavBar />
-            <img src={image} style={styles}></img>
+            <img src={image} className={styles.image}></img>
         </main>
     )
 }
